db80b5c966cceb60b6b2bbc9b9cdc07ff0ef33d9
wuguojun <wuguoju@zto.com>
2019-09-28 20:20:17 +0800

    权限管理

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    {{val()}}
+    <hah></hah>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Hah from './hahah'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  components: {
+    Hah
+  },
+  methods: {
+    val() {
+      console.log(23)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
